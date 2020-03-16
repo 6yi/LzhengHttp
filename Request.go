07f78af74f2eb9497d1949b9d951213beeb3e217
6yi <lzhengycy@outlook.com>
2020-03-16 20:13:13 +0800

    sec

--- Request.go
+++ Request.go
@@ -1,4 +1,5 @@
 package LzhengHttp
+
 import (
 	"io/ioutil"
 	"net/http"
@@ -9,11 +10,16 @@ type LzhengHttp struct {
 	head map[string]string
 	resp *http.Response
 	form url.Values
+	cookie []http.Cookie
 }
 func (l *LzhengHttp)SetHead(key,value string)  {
 	l.head[key]=value
 }
-func (l *LzhengHttp)SetParams(key,value string){
+func (l *LzhengHttp)SetCookie(cookie http.Cookie)  {
+	l.cookie = append(l.cookie,cookie)
+}
+
+func (l *LzhengHttp)SetParms(key,value string){
 	if l.form==nil{
 		l.form=make(url.Values)
 	}
@@ -21,7 +27,7 @@ func (l *LzhengHttp)SetParams(key,value string){
 }
 
 func InitHttpRequest()*LzhengHttp {
-	return &LzhengHttp{head:make(map[string]string),resp:nil,form:nil}
+	return &LzhengHttp{head:make(map[string]string),resp:nil,form:nil,cookie:make([]http.Cookie,0)}
 }
 
 func (l *LzhengHttp) Request(method,Url string,) (*http.Response,error){
@@ -35,10 +41,16 @@ func (l *LzhengHttp) Request(method,Url string,) (*http.Response,error){
 			req.Header.Set(k,v)
 		}
 	}
+	if len(l.cookie)!=0{
+		for _,v:=range l.cookie{
+			req.AddCookie(&v)
+		}
+	}
 	if l.form!=nil{
 		req.Body = ioutil.NopCloser(strings.NewReader(l.form.Encode()))
 	}
 	resp, err := client.Do(req)
 	l.resp=resp
+
 	return resp,err
 }
